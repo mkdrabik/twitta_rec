5f6a181b083166cc5eadbc7663b066e3196b19fa
Mason Drabik <mkdrabik@unc.edu>
2023-09-26 23:41:47 -0400

    d

---
+++ p01-xcode/p01-xcode/p01_xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  p01_xcodeApp.swift
+//  p01-xcode
+//
+//  Created by Mason Drabik on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct p01_xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
